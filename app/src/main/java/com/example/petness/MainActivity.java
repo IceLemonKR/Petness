14c3e763fd4a7078b320f9129c94d224cda87601
IceLemonKR <IceLemonKR@gmail.com>
2020-11-09 10:17:56 +0900

    New

--- app/src/main/java/com/example/petness/MainActivity.java
+++ app/src/main/java/com/example/petness/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     final static int BT_MESSAGE_READ = 2;
     final static int BT_CONNECTING_STATUS = 3;
     final static UUID BT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-
+    //Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
